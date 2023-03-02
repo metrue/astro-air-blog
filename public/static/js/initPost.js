dedd9dc2d684fbb21a36485081bdd942abaf8e7f
Alexander Phillips <bigtear@vip.qq.com>
2023-03-02 23:24:39 +0800

    Update initPost.js

--- public/static/js/initPost.js
+++ public/static/js/initPost.js
@@ -106,6 +106,10 @@ function syntaxHighlight() {
 
   script.onload = function () {
     // console.log("hljs.js loaded");
+    // 忽略未转义的HTML警告，自己的md文档默认可信
+    hljs.configure({
+      ignoreUnescapedHTML: true
+    });
     document.querySelectorAll('pre code').forEach((el) => {
       hljs.highlightElement(el);
     });
