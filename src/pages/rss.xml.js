4019315c70a273adac497fa4bea4f89a12ae717e
Lance Liao <llshu2003@gmail.com>
2023-03-17 17:08:49 +0800

    Use constants for RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,11 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import { SITE_TITLE, SITE_DESCRIPTION, SITE_URL } from '../consts.js';
 
 export async function get() {
   return rss({
-    title: "Austin's Blog",
-    description: "Site description",
-    site: 'https://astro-blog.qum.cc',
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: SITE_URL,
     items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
     customData: `<language>zh-cn</language>`,
   });
