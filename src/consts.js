e793931c44f96db6c31abcca12cec588cc8e7df0
Austin <lookcos@gmail.com>
2023-03-19 12:13:25 +0800

    PATCH: modified header and footer

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Austin's Blog`;
-export const SITE_DESCRIPTION = 'Austin Site Description';
+export const SITE_TITLE = `Air Blog`;
+export const SITE_DESCRIPTION = 'Site Description';
 export const SITE_EMAIL = 'no.sql@qq.com'
 export const SITE_NAME = 'astro.yufengbiji.com';
 export const SITE_URL = "https://astro.yufengbiji.com";
